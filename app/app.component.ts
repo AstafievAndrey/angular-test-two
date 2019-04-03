48fd49ab96ad6998f9fdd2f7db5812c313f916b3
AstafievAndrey <carrats@yandex.ru>
2019-04-03 18:45:40 +0300

    Переименовал методы.

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   public tabs = [ 1, 2 ];
 
-  public dec() {
+  public removeTab() {
     this.tabs = this.tabs.slice(0, -1);
   }
 
-  public inc() {
+  public addTab() {
     this.tabs = [ ...this.tabs, (this.tabs.length + 1) ];
   }
 }
